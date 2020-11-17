b94d5bcdad0e0447934dbe41507de3c34fb8e6c2
Einar Ingebrigtsen <einar@dolittle.com>
2020-11-17 15:02:50 +0100

    Putting in test

---
+++ Source/for_Projection/when_doing_things.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+describe('when doing things', () => {
+    it('should do stuff', () => true.should.be.true);
+})

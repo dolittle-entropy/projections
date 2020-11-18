44a13dc3abc256a813d78d6bb64a4f249051222c
Einar Ingebrigtsen <einar@dolittle.com>
2020-11-18 08:16:10 +0100

    Defining operation context

---
+++ Source/IOperationContext.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { Guid } from '@dolittle/rudiments';
+
+export interface IOperationContext {
+    identifier: Guid;
+    model: any;
+    events: any[];
+
+    getState(key: string): any;
+    setState(key: string, state: any);
+}

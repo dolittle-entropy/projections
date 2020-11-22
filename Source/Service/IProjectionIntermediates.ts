18b50814dad75fed7841bcae97fca21ff8cbb7d8
Einar Ingebrigtsen <einar@dolittle.com>
2020-11-22 09:26:48 +0100

    Removing unused types

--- Source/Service/IProjectionIntermediates.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Dolittle. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-export abstract class IProjectionIntermediates {
-    abstract get(id: any): Promise<any>;
-    abstract upsert(id: any, content: any): Promise<void>;
-}
--- Source/Service/IProjectionIntermediatesManager.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Dolittle. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-import { StreamId } from '@dolittle/sdk.events';
-import { IProjectionIntermediates } from './IProjectionIntermediates';
-
-export abstract class IProjectionIntermediatesManager {
-    abstract getFor(stream: StreamId, identifier: string): Promise<IProjectionIntermediates>;
-}
